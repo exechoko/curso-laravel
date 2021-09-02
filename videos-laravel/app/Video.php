54accdb501cbb40424927ca235082ac425de0aa0
Exequiel Marichal <exemarichal@gmail.com>
2021-09-02 17:53:05 -0300

    relacion entre entidades con orm eloquent

---
+++ videos-laravel/app/Video.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+//ORM Eloquent
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $table = 'videos';
+
+    // Relacion One to Many
+    public function comments(){
+      return $this->hasMany('App\Comment');
+    }
+
+    // Relacion de Muchos a Uno
+    public function user(){
+      return $this->belongsTo('App\User', 'user_id');
+    }
+}
